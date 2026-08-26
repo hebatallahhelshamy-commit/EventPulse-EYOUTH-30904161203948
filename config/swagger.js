dacfc0a749dcb7b7c2d42357ffb07505d5c792c8
Ahmed Elshamy <ahmed.h.elshamy2010@gmail.com>
2026-08-27 00:45:52 +0300

    Fix Swagger UI

--- config/swagger.js
+++ config/swagger.js
@@ -1,5 +1,4 @@
 const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 const path = require('path');
 
 const routesPath = path.resolve(__dirname, '../routes');
@@ -7,11 +6,13 @@ const routesPath = path.resolve(__dirname, '../routes');
 const options = {
   definition: {
     openapi: '3.0.0',
+
     info: {
       title: 'EventPulse API',
       version: '1.0.0',
       description: 'API Documentation for EventPulse Management Platform',
     },
+
     servers: [
       {
         url:
@@ -33,18 +34,46 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 const swaggerDocs = (app) => {
+  // OpenAPI JSON
   app.get('/api-docs.json', (req, res) => {
     res.json(swaggerSpec);
   });
 
-  app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, {
-      explorer: true,
-      customSiteTitle: 'EventPulse API Documentation',
-    })
-  );
+  // Swagger UI
+  app.get('/api-docs', (req, res) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>EventPulse API Documentation</title>
+
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css"
+        />
+      </head>
+
+      <body>
+        <div id="swagger-ui"></div>
+
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+
+        <script>
+          window.onload = function () {
+            SwaggerUIBundle({
+              url: '/api-docs.json',
+              dom_id: '#swagger-ui',
+              deepLinking: true
+            });
+          };
+        </script>
+      </body>
+      </html>
+    `);
+  });
 };
 
 module.exports = swaggerDocs;
